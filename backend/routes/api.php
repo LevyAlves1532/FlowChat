1257ed37f2f52c55c37a7f4c76081c27a26a7b92
Lêvy Alves <levy.pereiraA1532@gmail.com>
2025-09-17 07:38:13 -0300

    Colocando limite de requisição

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/v1')->group(function () {
+Route::prefix('/v1')->middleware('throttle:100,2')->group(function () {
     Route::middleware('auth:api')->group(function () {
         Route::apiResource('/auth', AuthController::class);
 
