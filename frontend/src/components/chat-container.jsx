44bac1cc3bcbae1af7a31b226808cd938cadd041
Lêvy Alves <levy.pereiraA1532@gmail.com>
2025-09-18 21:58:50 -0300

    Exibindo mensagens do chat

--- frontend/src/components/chat-container.jsx
+++ frontend/src/components/chat-container.jsx
@@ -4,9 +4,12 @@ import { useAuthStore } from "../store/useAuthStore";
 import { useChatStore } from "../store/useChatStore";
 
 import ChatHeader from "./chat-header";
+import NoChatHistoryPlaceholder from "./no-chat-history-placeholder";
+import MessageInput from "./message-input";
+import MessageLoadingSkeleton from "./message-loading-skeleton";
 
 function ChatContainer() {
-  const { selectedUser, getMessages, messages } = useChatStore();
+  const { selectedUser, getMessages, messages, isMessagesLoading } = useChatStore();
   const { authUser } = useAuthStore();
 
   useEffect(() => {
@@ -16,6 +19,39 @@ function ChatContainer() {
   return (
     <>
       <ChatHeader />
+      <div className="flex-1 px-6 overflow-y-auto py-8">
+        {messages.length > 0 && !isMessagesLoading ? (
+          <div className="max-w-3xl mx-auto space-y-6">
+            {messages.map((message) => (
+              <div 
+                className={`chat ${message.is_you ? "chat-end" : "chat-start"}`} 
+                key={message.id}
+              >
+                <div 
+                  className={`
+                    chat-bubble relative
+                    ${message.is_you 
+                        ? "bg-cyan-600 text-white"
+                        : "bg-slate-800 text-slate-200"}
+                  `}
+                >
+                  {message.image && (
+                    <img src={message.image} alt="Compartilhada" className="rounded-lg h-48 object-cover" />
+                  )}
+                  {message.text && <p className="mt-2">{message.text}</p>}
+                  <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
+                    {new Date(message.created_at).toISOString().slice(11, 16)}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : isMessagesLoading ? <MessageLoadingSkeleton /> : (
+          <NoChatHistoryPlaceholder name={selectedUser.name} />
+        )}
+      </div>
+
+      <MessageInput />
     </>
   )
 }
--- frontend/src/components/chat-header.jsx
+++ frontend/src/components/chat-header.jsx
@@ -1,8 +1,20 @@
 import { XIcon } from "lucide-react";
 import { useChatStore } from "../store/useChatStore"
+import { useEffect } from "react";
 
 function ChatHeader() {
   const { selectedUser, setSelectedUser } = useChatStore();
+
+  useEffect(() => {
+    const handleEscKey = (event) => {
+      if (event.key === "Escape") 
+        setSelectedUser(null);
+    }
+
+    window.addEventListener("keydown", handleEscKey)
+
+    return () => window.removeEventListener("keydown", handleEscKey);
+  }, [setSelectedUser])
 
   return (
     <div className="flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-[84px] px-6 flex-1">
